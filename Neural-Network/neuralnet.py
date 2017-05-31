875e78d8dcb2f01968a9266efac0cffd7b1896de
bkaperick <bryankaperick@gmail.com>
2017-05-31 17:54:18 -0400

    Reorganized code infrastructure for clarity.  Removed the Layer class altogether, which cleans up the handling of the input layer w.r.t. other layers.

--- Neural-Network/neuralnet.py
+++ Neural-Network/neuralnet.py
@@ -13,7 +13,7 @@ def sigmoid_deriv(z):
 
 def cost(output_emp, output_act):
     ''' Mean squared error between empirical and actual outputs '''
-    n = np.shape(output_emp)[1]
+    n = output_emp.shape[1]
     cost_val = 0
     
     for i in range(n):
@@ -39,25 +39,25 @@ class Network:
         layers - array of layer sizes, 
                 with layers[0] as input and layers[-1] as output
         '''
-        self.layer_sizes = layers
+        
+        assert(len(layers) > 1)
+        assert(len(layers)-1 == len(weights) == len(biases))
+        self.layers = layers
+        self.weights = weights
+        self.biases = biases
         self.num_layers = len(layers)
         self.input_dim = layers[0]
         self.output_dim = layers[-1]
-        self.init_layers(weights, biases)
-
-    def init_layers(self, weights, bias):
-        ''' Initialize the neurons, the layers, and their connections '''
-        self.layers = []
-        for i,layer in enumerate(self.layer_sizes):
-            if i == 0:
-                self.layers.append(InputLayer(self.layer_sizes[0]))
-            else:
-                self.layers.append(Layer(weights[i], bias[i]))
+        #self.init_layers(weights, biases)
+        print([x.shape for x in biases])
+        print(layers)
+        assert(all([biases[i].shape[0] == self.layers[i+1] for i in range(self.num_layers - 1)]))
+        assert(all([weights[i].shape == (self.layers[i+1],self.layers[i]) for i in range(0,self.num_layers-1)]))
     
     def update_layers(self, weights, bias):
         ''' Update weights and biases in the neuron layers '''
-        for i,layer in enumerate(self.layer_sizes):
-            self.layers.append(Layer(weights[i], bias[i]))
+        self.weights = weights
+        self.biases = bias
     
     def run(self, inputs, weights = None, biases = None, log = False):
         '''
@@ -75,16 +75,17 @@ class Network:
                 self.init_layers(weights, biases) 
         
         if log:
+            # Need to save each state layer output during training
             inputs = [inputs]
        
-        for i, layer in enumerate(self.layers):
+        for i in range(self.num_layers - 1):
             # Inputs for the next layer are precisely the 
             # outputs of this layer
             if log:
-                if i > 0:
-                    inputs.append(layer.evaluate(inputs[-1]))
+                #if i > 0:
+                inputs.append(self.eval_layer(i, inputs[-1]))
             else:
-                inputs = layer.evaluate(inputs)
+                inputs = self.eval_layer(i, inputs)
 
         return inputs
     
@@ -92,10 +93,12 @@ class Network:
 
         # Array containing a_0, ..., a_{L-1}
         layer_outputs = self.run(inputs, weights, biases, log = True)
+        assert(len(layer_outputs) == self.num_layers)
+        assert(all([layer_outputs[i].shape[0] == self.layers[i] for i in range(self.num_layers)]))
 
         # z_{L-1} = w_{L-1}*a_{L-2} + b_{L-1}
-        last_weighted_input = np.matmul(self.layers[-1].weight, layer_outputs[-2]) + self.layers[-1].bias[:,np.newaxis]
-        zs = [np.shape(last_weighted_input)]
+        last_weighted_input = np.matmul(self.weights[-1], layer_outputs[-2]) + self.biases[-1][:,np.newaxis]
+        zs = [last_weighted_input.shape]
 
         # delt_{L-1} = (a_{L-1} - y_{L-1}) * s'(z_{L-1})
         delta_lplus1 = self.cost_gradient(layer_outputs[-1], correct_outputs[-1]) * sigmoid_deriv(last_weighted_input)
@@ -110,11 +113,14 @@ class Network:
         for l in range(self.num_layers-2,0,-1):
 
             # z_{L-2} = w_{L-2} * a_{L-3} + b_{L-2}
-            weighted_input = np.matmul(self.layers[l].weight, layer_outputs[l-1]) + self.layers[l].bias[:,np.newaxis]
-            zs.insert(0,np.shape(weighted_input))
+            # w[self.num_layers-2], layer_outputs[self.num_layers-2] + 
+            #print(len(self.weights), len(layer_outputs), len(self.biases[5]))
+            weighted_input = np.matmul(self.weights[l-1], layer_outputs[l-1]) + self.biases[l-1][:,np.newaxis]
+            zs.insert(0,weighted_input.shape)
 
             # delt_{L-2} = w_{L-1}^T * delt_{L-1} * s'(z_{L-2})
-            delta_l = np.matmul(np.transpose(self.layers[l+1].weight), delta_lplus1) 
+            # delt_1 = w_2^T * delt_2 * s'(z_0)
+            delta_l = np.matmul(np.transpose(self.weights[l]), delta_lplus1) 
             
             # Element-wise multiplication by s'(z_{L-2})
             delta_l *= sigmoid_deriv(weighted_input)
@@ -142,19 +148,19 @@ class Network:
             if j % 50 == 0:
                 print("{0} epochs completed".format(j))
             
-            weight_grads = [np.zeros(np.shape(l.weight)) for l in self.layers[1:]]
-            bias_grads = [np.zeros(np.shape(l.bias)) for l in self.layers[1:]]
+            weight_grads = [np.zeros(weight.shape) for weight in self.weights]
+            bias_grads = [np.zeros(bias.shape) for bias in self.biases]
             
             # Take only a subset
-            indices = random.sample(range(np.shape(train_inputs)[1]), batch_size)
+            indices = random.sample(range(train_inputs.shape[1]), batch_size)
             batch_inputs = train_inputs[:,indices]
             batch_outputs = train_outputs[:,indices]
             
             weight_grads, bias_grads = self.back_propagate(batch_inputs, batch_outputs)
 
             for i,layer in enumerate(self.layers[1:]):
-                layer.weight = layer.weight - eta* weight_grads[i] / batch_size
-                layer.bias = layer.bias - eta*bias_grads[i] / batch_size
+                self.weights[i] = self.weights[i] - eta* weight_grads[i] / batch_size
+                self.biases[i] = self.biases[i] - eta*bias_grads[i] / batch_size
             
             if len(test_inputs) > 0 and j % tick == 0:
                 cost = self.cost(test_inputs, test_outputs) 
@@ -165,7 +171,7 @@ class Network:
             print("plotting")
             x,y = zip(*cost_points)
             fig = plt.plot(x,y)
-            plt.title("Layers: {0}, Training Size: {1}, Epochs: {2}, Learning Rate: {3}".format(self.layer_sizes, len(train_inputs), epochs, eta))
+            plt.title("Layers: {0}, Training Size: {1}, Epochs: {2}, Learning Rate: {3}".format(self.layers, len(train_inputs), epochs, eta))
             plt.ylabel("Quadratic Cost")
             plt.xlabel("Epoch")
             plt.ylim([0,max([c for (_,c) in cost_points])])
@@ -178,6 +184,7 @@ class Network:
         compute MSE of the difference in the actual
         and expected outputs -
         inputs - each column is a new input sample
+
         '''
         
         # Feed forward with inputs, weights and biases
@@ -188,27 +195,10 @@ class Network:
         
     def cost_gradient(self, output_emp, output_act):
         return cost_gradient(output_emp, output_act)
-
-
-class Layer:
-    ''' Encodes a layer of Sigmoid neurons with a set of weights and biases '''
-    def __init__(self, weight, bias):
-        self.length = len(bias)
-        self.bias = bias
-        self.weight = weight
-        self.__name__ = "Layer"
-
-    def evaluate(self, inputs):
+    
+    def eval_layer(self, l, inputs):
         # Note adding a 1D ndarray to a 2D ndarray results in adding the i^th component
         # to the i^th column of the 2D ndarray
-        return sigmoid(np.matmul(self.weight, inputs) + self.bias[:,np.newaxis])
-
-
-class InputLayer(Layer):
-    ''' Subclass of Layer whos evaluation is just the identity mapping '''
-    def __init__(self, length):
-        self.length = length
-        self.__name__ = "InputLayer"
-
-    def evaluate(self, inputs):
-        return inputs
+        #if l == 0:
+        #    return inputs
+        return sigmoid(np.matmul(self.weights[l], inputs) + self.biases[l][:,np.newaxis])
--- Neural-Network/test.py
+++ Neural-Network/test.py
@@ -3,23 +3,28 @@ from neuralnet import *
 
 # 28x28 pixel images are encoded as a length-784 numpy array
 # Outputs are a length-10 numpy array corresponding to 10 decimal digits
-layer_map = [784,25,5,5,5,10]
+layer_map = [784,30,10]
 
 
 # Load data
-num_samples = 5000
-training_labels, training_images, test_labels, test_images = open_data.get_data(num_samples, training=True, test=True)
+num_samples = 60000
+training_labels, training_images, test_labels, test_images = open_data.get_data(num_samples, training=True, test=False)
+
+training_labels = training_labels[:50000]
+training_images = training_images[:50000]
+test_labels = training_labels[50000:]
+test_images = training_images[50000:]
 
 
 # Initialize weights randomly
-weights = [None]
+weights = []
 for i,size in enumerate(layer_map[:-1]):
     next_size = layer_map[i+1]
     weights.append(np.random.rand(next_size, size))
 
 print([np.shape(w) for w in weights])
 
-biases = [None]
+biases = []
 for i,size in enumerate(layer_map[1:]):
     
     biases.append(np.random.randn(size))
@@ -37,9 +42,9 @@ def label_to_flag_vec(labels):
 training_label_vecs = label_to_flag_vec(training_labels)
 test_label_vecs = label_to_flag_vec(test_labels)
 
-epochs = 50
-eta = .1
-batch_size = 50
+epochs = 30
+eta = 3
+batch_size = 10
 
 # Train network weights and labels on training data
 network.stochastic_gradient_descent(training_images, training_label_vecs, batch_size, epochs, eta, test_images, test_label_vecs, tick=1, plot=True)
